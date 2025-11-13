ecb9f353dc9fb93a5c97693393bce6f124a98552
Bomjan <sundrabomjan@gmail.com>
2025-11-13 11:33:12 +0600

    keyboard page layout

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -3,11 +3,19 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import keyboard from "../../assets/dkeybr.png";
 import { IoFlame } from "react-icons/io5";
 import { VscDebugRestart } from "react-icons/vsc";
+import { useState } from "react";
 
 import PauseResume from "../Buttons/PauseResume/PauseResume";
 export default function Keyboard() {
   const wordCount = 200;
   const streak = 10;
+  const text = `
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+    similique, architecto molestiae temporibus culpa iusto officiis ab
+    perferendis amet atque perspiciatis rem dolores soluta reprehenderit
+    consequatur. Perspiciatis quos optio omnis?
+  `;
+  const [isFocused, setIsFocused] = useState(false);
   return (
     <main className={style.main}>
       <div className={style.container}>
@@ -23,12 +31,22 @@ export default function Keyboard() {
             <VscDebugRestart className={style.restart} />
           </div>
         </div>
+        <div className={style.teatArea}>
+          {!isFocused ? (
+            <div className={style.cover} onClick={() => setIsFocused(true)}>
+              <p>Click here to START typing</p>
+            </div>
+          ) : (
+            <div className={style.text}>{text}</div>
+          )}
+        </div>
         <div className={style.keyboard}>
           <LazyLoadImage
             src={keyboard}
             alt="Keyboard Image"
             height={450}
             width={1200}
+            className={style.keyboardImage}
           />
         </div>
       </div>
